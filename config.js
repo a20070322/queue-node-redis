0621d944be431e16f2d5c38d12e5b2032db6490d
王真真 <1031839775@qq.com>
2022-07-18 22:48:41 +0800

    create config

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // redis 配置
+  redis: {
+    default: {
+      host: "127.0.0.1",
+      port: 6379,
+      password: "",
+      db: 0,
+    },
+  },
+  // 消息队列频道设置
+  mqList: [
+    {
+      // 消息频道名称
+      name: "QUEUE_MY_MQ",
+      // 阻塞式取值超时配置
+      brPopTimeout: 100,
+      // 开启任务数 此配置需要 PM 启动生效
+      instances: 1,
+      // redis 配置key
+      redis: "default",
+    },
+  ],
+};
